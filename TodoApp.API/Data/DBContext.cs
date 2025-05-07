7dd606b346dc13d2f02a7a152d8d703262ef16ae
Varsha kushwah <varshakushwah711@gmail.com>
2025-05-07 16:57:00 +0530

    Initial commit

---
+++ TodoApp.API/Data/DBContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApp.API.Models;
+
+namespace TodoApp.API.Data
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) {}
+
+        public DbSet<TodoItem> TodoItems { get; set; }  = null!;
+    }
+}
---
+++ TodoApp.API/Program.cs
@@ -0,0 +1,30 @@
+using TodoApp.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+//Creating the Builder:
+var builder = WebApplication.CreateBuilder(args);
+
+//Adding DB Context:
+builder.Services.AddDbContext<TodoContext>(options => //lambda expression
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+});
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+app.UseAuthorization();
+
+app.MapControllers();
+app.Run();
